c9cbd380b82ecb8c3d383a71728975e0ec05868e
Anton <a.chertok@akatech.com.ua>
2024-02-11 10:19:07 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,14 @@ import App from "./components/App";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { HelmetProvider } from "react-helmet-async";
 import globalStyles from "styles/globalStyles";
+import { theme } from "styles/theme";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={}>
+    <ThemeProvider theme={theme}>
       <HelmetProvider>
         <CssBaseline />
         {globalStyles}
